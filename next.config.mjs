36a4f174623c894512bdc3f0f0daac812a68ba36
Jonathan Koh <jonathan.koh75@gmail.com>
2025-06-04 15:25:39 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
     basePath: "/personal-web-portfolio-2024",
-    trailingSlash: true,
 };
 
 export default nextConfig;
